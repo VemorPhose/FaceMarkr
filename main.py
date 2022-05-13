c6bab7144ce277edd382ec3f6cd348183fb8f3e5
VemorPhose <63329229+VemorPhose@users.noreply.github.com>
2022-05-14 02:29:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,18 +4,22 @@ import face_recognition
 import os
 from datetime import datetime
 
+############################################################ encodeKnown block
 path = 'imgbase'
 imagesKnown = []
 classNames = []
 myList = os.listdir(path)
-print(myList)
+# print(myList)
+
+# read images from folder
 for cl in myList:
     currentImg = cv2.imread(f'{path}/{cl}')
     imagesKnown.append(currentImg)
     classNames.append(os.path.splitext(cl)[0])
-print(classNames)
+    # print(classNames)
 
 
+# findEncodings
 def findEncodings(images):
     encodeList = []
     for img in images:
@@ -25,25 +29,89 @@ def findEncodings(images):
     return encodeList
 
 
-def markAttendance(name):
-    f = open('Attendance.csv', 'r+')
-    DataList = f.readlines()
-    nameList = []
-    for line in DataList:
-        entry = line.split(',')
-        nameList.append(entry[0])
-    if name not in nameList:
-        now = datetime.now()
-        timestr = now.strftime('%H:%M:%S')
-        f.writelines(f'{name},{timestr}\n')
-
-
 encodeListKnown = findEncodings(imagesKnown)
 print('Encoding Complete')
 
+################################################################ csv file handling block
+g = open('Attendance.csv', 'r+')
+DataList = g.readlines()
+nameList = []
+
+# sort bullshit from csv
+for i in range(len(DataList)):
+    if DataList[-i - 1] == '\n':
+        DataList.pop(-i - 1)
+
+# print(DataList)
+
+headerList = DataList[0].split(',')
+headerList[-1] = headerList[-1].split()[0]
+DataList.pop(0)
+
+# nameList creation from DataList
+for line in DataList:
+    line = line.replace('\n', '')
+    entry = line.split(',')
+    nameList.append(entry)
+
+# print(nameList)
+
+# modifying header for csv
+headerStr = f'{headerList[0]},{headerList[1]}'
+
+for i in range(len(headerList) - 2):
+    headerStr += f',{headerList[i + 2]}'
+if headerList[-1] != f'{datetime.now().strftime("%d/%m/%y")}':
+    headerStr += f',{datetime.now().strftime("%d/%m/%y")}\n'
+else:
+    headerStr += f'\n'
+
+g.seek(0)
+g.writelines(headerStr)
+
+# appending nameList
+if len(nameList[0]) < len(headerStr.split(',')):
+    for i in range(len(nameList)):
+        nameList[i].append('A')
+
+
+# print(nameList)
+
+
+def rewrite():
+    global headerStr
+    global nameList
+    g.seek(0)
+    g.writelines(headerStr)
+
+    for i in range(len(nameList)):
+        tempStr = f'{nameList[i][0]},{nameList[i][1]}'
+        for j in range(len(nameList[i]) - 3):
+            tempStr += f',{nameList[i][j + 3]}'
+        tempStr += f',{nameList[i][-1]}\n'
+        g.writelines(tempStr)
+
+
+def markAttendance(name):
+    global DataList
+    global nameList
+
+    for n in range(len(nameList)):
+        if nameList[n][1] == name:
+            nameList[n][-1] = 'P'
+            break
+
+    rewrite()
+
+
+rewrite()
+
+############################################################## live video feed block
 cap = cv2.VideoCapture(0)
 
 while True:
+    # timeNow = datetime.now()
+
     success, img = cap.read()
     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
